cacc44687c92306524c48997f2fe2481ba8c1a53
FarhatAsharfillah <farhatasharfillah03@gmail.com>
2025-10-31 21:35:10 +0700

    first commit menambahkan file strings.xml di values dan SubmissionForm.kt

---
+++ app/src/main/java/com/example/submissionformui/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.example.submissionformui
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.submissionformui.ui.theme.SubmissionFormUITheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            SubmissionFormUITheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    greeting(
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    SubmissionFormUITheme {
+        Greeting("Android")
+    }
+}
