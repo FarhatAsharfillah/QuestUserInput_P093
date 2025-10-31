340c376f575c918890257c09f9d269ac10fe991b
FarhatAsharfillah <farhatasharfillah03@gmail.com>
2025-10-31 22:07:07 +0700

    menambahkan fungsi dasar submissionform beserta imports yg dibutuhkan

--- app/src/main/java/com/example/submissionformui/SubmissionForm.kt
+++ app/src/main/java/com/example/submissionformui/SubmissionForm.kt
@@ -1,4 +1,19 @@
 package com.example.submissionformui
 
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 
+@Composable
+fun SubmissionForm() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+    ) {
+        // Form content will be here
+    }
+}
